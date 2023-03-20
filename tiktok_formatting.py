5f495a16770c3538b8c2b01adf7e1c5ebf1d541f
Caesar Tuguinay <cmtuguinay2000@gmail.com>
2023-03-19 23:00:13 -0700

    formatting prototype and auto_upload failed attempt

--- tiktok_formatting.py
+++ tiktok_formatting.py
@@ -14,20 +14,18 @@ def format_video(input_video_path, output_video_path):
     # Load the input video clip using VideoFileClip
     video_clip = VideoFileClip(input_video_path)
 
-    # Resize the video clip to 1080x1920 using resize
-    resized_clip = resize(video_clip, (width, height))
-
     # If the aspect ratio of the input video clip is different from 9:16 (1080x1920), crop the video clip to fit
     if video_clip.aspect_ratio != 9/16:
-        cropped_clip = crop(resized_clip, width=width, height=height)
+        cropped_clip = crop(video_clip, width=width, height=height)
     else:
-        cropped_clip = resized_clip
+        cropped_clip = video_clip
 
     # Create a color clip with the same dimensions as the video clip using ColorClip
     background_clip = ColorClip(size=(width, height), color=background_color)
 
     # Composite the cropped video clip and the background clip using CompositeVideoClip
     final_clip = CompositeVideoClip([background_clip, cropped_clip.set_pos("center")])
+    final_clip.duration = video_clip.duration
 
     # Write the final clip to a new file using write_videofile
     final_clip.write_videofile(output_video_path)
@@ -36,8 +34,7 @@ def format_video(input_video_path, output_video_path):
     video_clip.close()
     final_clip.close()
 
-
 if __name__ == "__main__":
-    input_video_path = "PATH_TO_INPUT_VIDEO.mp4"
-    output_video_path = "PATH_TO_OUTPUT_VIDEO.mp4"
+    input_video_path = "data/videos/03_18_23_sillycats.mp4"
+    output_video_path = input_video_path.replace(".mp4", "_final.mp4")
     format_video(input_video_path, output_video_path)
